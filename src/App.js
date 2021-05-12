66dc3e5ac592722cde104afe858cb882a02d1fe6
ermias kidane <hadeklte21@gmail.com>
2021-05-12 16:17:02 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import Home from "./components/home"
-import Lists from "./components/Lists"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import './App.css';
 
@@ -10,7 +9,6 @@ const App = () => {
     <Router>
         <Switch >
           <Route path="/"  component={Home}/>
-          <Route path="/lists"  component={Lists}/>
         </Switch>
     </Router>
     </>
